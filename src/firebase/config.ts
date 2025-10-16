131830252ac8ac2938875b383a64c95a953978c3
Gorilla Facts <inquiry.gorillafacts@gmail.com>
2025-10-16 08:33:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-136309706-daca7",
+  "appId": "1:700053322647:web:830a5283743ce552a062d2",
+  "apiKey": "AIzaSyBvn0mo967C5E-gQUEKqipIxyvlEO2HRBI",
+  "authDomain": "studio-136309706-daca7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "700053322647"
+};
